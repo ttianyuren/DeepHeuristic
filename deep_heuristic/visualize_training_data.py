e99bde69e96b1475e21ddaaf8b0b090fd1720852
Wahba <hassan-a-w@live.com>
2021-05-24 00:03:25 +0200

    update split_data params

--- deep_heuristic/visualize_training_data.py
+++ deep_heuristic/visualize_training_data.py
@@ -5,19 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def split_data(raw_data, test_size=0.1):
-    raw_data = np.array(raw_data, dtype=object)
+def split_data(raw_data, test_size=0.1, num_of_param=1):
+    raw_data = np.array(raw_data)
     np.random.shuffle(raw_data)
     data = np.array([])
     for i in raw_data:
-        data = np.append(data, np.array(i[0], dtype=np.float))
-    data = np.reshape(data, (-1, 3))
-    train_length = int(len(data) * (1-test_size))
+        data = np.append(data, np.array(i[0]))
+    data = np.reshape(data, (-1, num_of_param))
+    train_length = int(len(data) * (1 - test_size))
     train_data_in = data[:train_length]
     train_labels_in = raw_data[:train_length, 1]
     eval_data_in = data[train_length:]
     eval_labels_in = raw_data[train_length:, 1]
-    return train_data_in, train_labels_in.astype('bool'), eval_data_in, eval_labels_in.astype('bool')
+    return np.array(train_data_in).reshape(-1, num_of_param), np.array(train_labels_in, dtype=float), \
+        np.array(eval_data_in).reshape(-1, num_of_param), np.array(eval_labels_in, dtype=float)
+
 
 """
 file_reach = '../training_data/reach.pk'
