013d3c94198e60171df3f76b57db8321b736c052
Wahba <hassan-a-w@live.com>
2021-05-24 00:01:48 +0200

    added 3 diffrent NN to solve the same problem

---
+++ deep_heuristic/models/03_NN_Classifier.py
@@ -0,0 +1,142 @@
+from deep_heuristic.visualize_training_data import split_data
+import pickle as pk
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+file_reach = '../training_data/reach.pk'
+with open(file_reach, 'rb') as f:
+    all_data = pk.load(f)
+
+train_data, train_labels, eval_data, eval_labels = split_data(all_data, test_size=0.2, num_of_param=3)
+
+##################################################### Build Model #####################################################
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(train_data)
+X_test = scaler.transform(eval_data)
+
+EPOCHS = 50
+BATCH_SIZE = 64
+LEARNING_RATE = 0.0001
+
+
+## train data
+class trainData(Dataset):
+
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+
+train_data = trainData(torch.FloatTensor(X_train),
+                       torch.FloatTensor(train_labels))
+
+
+## test data
+class testData(Dataset):
+
+    def __init__(self, X_data):
+        self.X_data = X_data
+
+    def __getitem__(self, index):
+        return self.X_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+
+test_data = testData(torch.FloatTensor(X_test))
+
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=1)
+
+# Binary classification
+class BinaryClassification(nn.Module):
+    def __init__(self):
+        super(BinaryClassification, self).__init__()
+        # Number of input features is 12.
+        self.layer_1 = nn.Linear(3, 24)
+        self.layer_2 = nn.Linear(24, 24)
+        self.layer_out = nn.Linear(24, 1)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(24)
+        self.batchnorm2 = nn.BatchNorm1d(24)
+
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+        return x
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = BinaryClassification()
+model.to(device)
+print(model)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum / y_test.shape[0]
+    acc = torch.round(acc * 100)
+
+    return acc
+
+# 3) Training loop
+model.train()
+for e in range(1, EPOCHS + 1):
+    epoch_loss = 0
+    epoch_acc = 0
+    for X_batch, y_batch in train_loader:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+        optimizer.zero_grad()
+
+        y_pred = model(X_batch)
+
+        loss = criterion(y_pred, y_batch.unsqueeze(1))
+        acc = binary_acc(y_pred, y_batch.unsqueeze(1))
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} | Acc: {epoch_acc / len(train_loader):.3f}')
+
+y_pred_list = []
+model.eval()
+with torch.no_grad():
+    for X_batch in test_loader:
+        X_batch = X_batch.to(device)
+        y_test_pred = model(X_batch)
+        y_test_pred = torch.sigmoid(y_test_pred)
+        y_pred_tag = torch.round(y_test_pred)
+        y_pred_list.append(y_pred_tag.cpu().numpy())
+
+y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+confusion_matrix(eval_labels, y_pred_list)
+print(classification_report(eval_labels, y_pred_list))
