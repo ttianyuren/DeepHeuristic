83c35b8d254687d99e90582d519aeac4451f6afa
HassanWahba <47371151+HassanWahba@users.noreply.github.com>
2021-06-11 15:33:26 +0200

    Classifier Update (Rot90)

--- deep_heuristic/models/08_CNN_Classifier.py
+++ deep_heuristic/models/08_CNN_Classifier.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
-EPOCHS = 5000
+EPOCHS = 10
 BATCH_SIZE = 64
 LEARNING_RATE = 0.001
 
@@ -27,6 +27,8 @@ ny_dataset = np.array([])
 for item in load_collision():
     item = np.array(item)
     tmp = np.array(item[0])
+    if tmp[3] < 0.14:
+        continue
     if tmp[0] == 0:
         x_dataset = np.append(x_dataset, (tmp[1:5], item[1]))
     elif tmp[0] == 1:
@@ -46,29 +48,53 @@ nx_dataset = nx_dataset.reshape(-1, 2)
 ny_dataset = ny_dataset.reshape(-1, 2)
 
 # split each dataset to train and evaluate
-x_train_data, x_train_labels, x_eval_data, x_eval_labels = split_data(x_dataset, test_size=0.1, num_of_param=4)
-y_train_data, y_train_labels, y_eval_data, y_eval_labels = split_data(y_dataset, test_size=0., num_of_param=4)
-z_train_data, z_train_labels, z_eval_data, z_eval_labels = split_data(z_dataset, test_size=0.1, num_of_param=4)
-nx_train_data, nx_train_labels, nx_eval_data, nx_eval_labels = split_data(nx_dataset, test_size=0.1, num_of_param=4)
-ny_train_data, ny_train_labels, ny_eval_data, ny_eval_labels = split_data(ny_dataset, test_size=0.1, num_of_param=4)
+x_train_data, x_train_labels, x_eval_data, x_eval_labels = split_data(x_dataset, test_size=0.01, num_of_param=4)
+y_train_data, y_train_labels, y_eval_data, y_eval_labels = split_data(y_dataset, test_size=0.01, num_of_param=4)
+z_train_data, z_train_labels, z_eval_data, z_eval_labels = split_data(z_dataset, test_size=0.01, num_of_param=4)
+nx_train_data, nx_train_labels, nx_eval_data, nx_eval_labels = split_data(nx_dataset, test_size=0.01, num_of_param=4)
+ny_train_data, ny_train_labels, ny_eval_data, ny_eval_labels = split_data(ny_dataset, test_size=0.01, num_of_param=4)
 
 # load images
 x_train_pics = np.array([x[-8:, 16:48] for x in x_train_data[:, 3]]).reshape(-1, 256)
 x_eval_pics = np.array([x[-8:, 16:48] for x in x_eval_data[:, 3]]).reshape(-1, 256)
-y_train_pics = np.array([x[16:48, -8:] for x in y_train_data[:, 3]]).reshape(-1, 256)
-print(x_train_pics.shape)
-print(x_train_labels.shape)
-print(y_train_pics.shape)
-x_train_pics = np.append(x_train_pics, np.flip(y_train_pics.transpose(), axis=1)).reshape(-1, 256)
-print(x_train_pics.shape)
-x_train_labels = np.append(x_train_labels, y_train_labels)
-print(x_train_labels.shape)
-y_eval_pics = np.array([x[16:48, -8:] for x in y_eval_data[:, 3]]).reshape(-1, 256)
-z_train_pics = [x[16:48, 16:48] for x in z_train_data[:, 3]]
-nx_train_pics = np.array([x[:16, 16:48] for x in nx_train_data[:, 3]]).reshape(-1, 512)
-nx_eval_pics = np.array([x[:16, 16:48] for x in nx_eval_data[:, 3]]).reshape(-1, 512)
-ny_train_pics = [x[16:48, :16] for x in ny_train_data[:, 3]]
 
+y_train_pics = np.array([np.rot90(x, -1)[-8:, 16:48] for x in y_train_data[:, 3]]).reshape(-1, 256)
+y_eval_pics = np.array([np.rot90(x, -1)[-8:, 16:48] for x in y_eval_data[:, 3]]).reshape(-1, 256)
+
+nx_train_pics = np.array([np.rot90(x, 2)[-8:, 16:48] for x in nx_train_data[:, 3]]).reshape(-1, 256)
+nx_eval_pics = np.array([np.rot90(x, 2)[-8:, 16:48] for x in nx_eval_data[:, 3]]).reshape(-1, 256)
+
+ny_train_pics = np.array([np.rot90(x, 1)[-8:, 16:48] for x in ny_train_data[:, 3]]).reshape(-1, 256)
+ny_eval_pics = np.array([np.rot90(x, 1)[-8:, 16:48] for x in ny_eval_data[:, 3]]).reshape(-1, 256)
+
+x_train_pics = np.append(x_train_pics, y_train_pics).reshape(-1, 256)
+x_train_pics = np.append(x_train_pics, nx_train_pics).reshape(-1, 256)
+x_train_pics = np.append(x_train_pics, ny_train_pics).reshape(-1, 256)
+
+x_train_labels = np.append(x_train_labels, y_train_labels)
+x_train_labels = np.append(x_train_labels, nx_train_labels)
+x_train_labels = np.append(x_train_labels, ny_train_labels)
+
+x_eval_pics = np.append(x_eval_pics, y_eval_pics).reshape(-1, 256)
+x_eval_pics = np.append(x_eval_pics, nx_eval_pics).reshape(-1, 256)
+x_eval_pics = np.append(x_eval_pics, ny_eval_pics).reshape(-1, 256)
+
+x_eval_labels = np.append(x_eval_labels, y_eval_labels)
+x_eval_labels = np.append(x_eval_labels, nx_eval_labels)
+x_eval_labels = np.append(x_eval_labels, ny_eval_labels)
+
+x_eval_pics = np.append(x_eval_pics, y_train_pics).reshape(-1, 256)
+x_eval_pics = np.append(x_eval_pics, nx_train_pics).reshape(-1, 256)
+x_eval_pics = np.append(x_eval_pics, ny_train_pics).reshape(-1, 256)
+
+x_eval_labels = np.append(x_eval_labels, y_train_labels)
+x_eval_labels = np.append(x_eval_labels, nx_train_labels)
+x_eval_labels = np.append(x_eval_labels, ny_train_labels)
+
+print(x_train_pics.shape)
+"""
+z_train_pics = np.array([x[16:48, 16:48] for x in z_train_data[:, 3]]).reshape(-1, 1024)
+"""
 ## train data and test data
 scaler = StandardScaler()
 X_train = scaler.fit_transform(x_train_pics)
